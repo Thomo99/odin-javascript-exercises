c87919853a207463f5c5cdb52fa9465612abd95a
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-18 22:02:44 +1100

    All exercises from Fundamentals Part 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,29 @@
-const removeFromArray = function(array, toRemove) {
-    index = array.indexOf(toRemove);
-    array.splice(index, 1);
+const removeFromArray = function(array, ...args) {
+//     let newArray = [];
+//     for (i in array) {
+//         if(!args.includes(i)){
+//             newArray.push(i);
+//         }
+//     }
 
-    return array;
+//     return newArray;
+const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
+
 };
+// let arr = [1,2,3,4]
+// removeFromArray(, 3);
+// console.log()
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+      return 'ERROR';  
+    }
+    let total = 0;
+    if (num1<num2){
+        while (num1 <=num2){
+            total+=num1;
+            num1++;
+        }
+    } else if (num1>num2){
+        while (num2 <=num1){
+            total+=num2;
+            num2++;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
