8b6e1ae2d1361348adeb66af9c8f967490d209da
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-25 05:48:07 +1100

    Up to power function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,22 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr = 0) {
+	let total = 0;
+
+  for (let i = 0; i < arr.length; i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((acc, curr) => acc * curr, 1);
 };
 
 const power = function() {
