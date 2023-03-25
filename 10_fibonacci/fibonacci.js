e2b45b97a6d61e06066e6f08c7bc51072da6018e
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-25 13:59:30 +1100

    Exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    Number(num);
+    if (num < 0){
+        return 'OOPS';
+    }
+    if (num === 1 || num ===2){
+        return 1;
+    }
+    let first = 0;
+    let second = 1;
+    let total = 1;
+    count = 1;
+    while (count < num){
+        total = first + second;
+        first = second;
+        second = total;
+        count += 1;
+    }
+    return total;
 };
 
 // Do not edit below this line
